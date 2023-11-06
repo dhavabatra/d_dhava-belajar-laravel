393bf56b00fcb562c212921c36deeb651feb968c
Dhava Batra <dhavabatra@gmail.com>
2023-11-07 03:39:57 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dhava's Site</title>
+
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <h1>Hello World</h1>
+    <div class="container">
+      <h1>Biodata Diri</h1>
+      <div class="info">
+        <h2>Data Pribadi</h2>
+        <p>Nama Lengkap: Dhava Bagus saputra</p>
+        <p>Tempat, Tanggal Lahir: Banyuwangi, 1 Juni 2003</p>
+        <p>Alamat: Kendal - Sumberbaru - Singojuruh - Banyuwangi</p>
+        <p>Email: dhavabatra@email.com</p>
+        <h2>Pendidikan</h2>
+        <ul>
+          <li>SMA Negeri 2 TARUNA BHAYANGKARA (2019-2021)</li>
+          <li>Politeknik Negeri Banyuwangi, Teknologi Rekayasa Perangkat Lunak (2021-now)</li>
+        </ul>
+      </div>
+    </div>
+</body>
+</html>
